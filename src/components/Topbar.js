86b177c8ac41e32a2b95c18be2476d9cbac43b7f
Jolynn Cuylle <jolynn.cuylle@gmail.com>
2024-10-16 16:46:37 +0200

    remove profile pic topbar from homepage

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Image } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth'; // Gebruik de react-firebase-hooks
 import { auth, db } from '../firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
 import './Topbar.css'; // Zorg voor de juiste CSS
@@ -9,11 +10,11 @@ import logo from '../assets/logo.png'; // Importeer het logo
 function TopBar() {
   const [userName, setUserName] = useState('');
   const [profilePic, setProfilePic] = useState('');
+  const [user, loading, error] = useAuthState(auth); // Gebruik de useAuthState hook
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const user = auth.currentUser;
       if (user) {
         try {
           const userDoc = await getDoc(doc(db, 'users', user.uid));
@@ -29,21 +30,27 @@ function TopBar() {
     };
 
     fetchUserData();
-  }, []);
+  }, [user]);
+
+  if (loading) {
+    return null; // Je kunt hier een spinner of laadscherm tonen als je wilt
+  }
 
   return (
     <div className="top-bar">
       <div className="logo" onClick={() => navigate('/')}>
-        <img src={logo} alt="App Logo" className="logo-img" /> {/* Toon je logo */}
+        <img src={logo} alt="App Logo" className="logo-img" />
       </div>
-      <div className="user-info">
-        {profilePic ? (
-          <Image src={profilePic} roundedCircle className="profile-pic" />
-        ) : (
-          <Image src="/path/to/default-profile.png" roundedCircle className="profile-pic" />
-        )}
-        <span className="user-name">{userName}</span>
-      </div>
+      {user && ( // Toon gebruikersinformatie alleen als de gebruiker is ingelogd
+        <div className="user-info">
+          {profilePic ? (
+            <Image src={profilePic} roundedCircle className="profile-pic" />
+          ) : (
+            <Image src="/path/to/default-profile.png" roundedCircle className="profile-pic" />
+          )}
+          <span className="user-name">{userName}</span>
+        </div>
+      )}
     </div>
   );
 }
